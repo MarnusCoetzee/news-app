014f6b39ff154f0bbe8c9181f913c2b8dce879e7
Marnus Coetzee <marnuscoe@gmail.com>
2022-08-29 13:28:04 +0200

    add http call for weather service and add bootstrap

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { filter, map, mergeMap, switchMap, toArray } from 'rxjs/operators';
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+interface OpenWeatherResponse {
+  list: {
+    dt_txt: string;
+    main: {
+      temp: number;
+    };
+  }[];
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ForecastService {
+  private url = 'https://api.openweathermap.org/data/2.5/forecast';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getCurrentLocation() {
+    return new Observable<GeolocationCoordinates>((observer) => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete();
+        },
+        (err) => observer.error(err)
+      );
+    });
+  }
+
+  getForecast() {
+    return this.getCurrentLocation().pipe(
+      map((coords) => {
+        return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'metric')
+          .set('appid', '8abf8c42136e3a66dda865d49e5acba8');
+      }),
+      switchMap((params) =>
+        this.http.get<OpenWeatherResponse>(this.url, { params })
+      ),
+      map((response) => response.list),
+      mergeMap((value) => of(...value)),
+      filter((value, index) => index % 8 === 0),
+      map((value) => {
+        return {
+          dateString: value.dt_txt,
+          temp: value.main.temp,
+        };
+      }),
+      toArray()
+    );
+  }
 }
--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ForecastService } from '../forecast.service';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.scss']
+  styleUrls: ['./forecast.component.scss'],
 })
 export class ForecastComponent implements OnInit {
+  forecastData$: Observable<{ dateString: string; temp: number }[]> =
+    this.forecastService.getForecast();
+  constructor(private forecastService: ForecastService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
